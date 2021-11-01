2513f0fc1f2956c1f58ae80b06ed782689d00012
Seqat <sedatalizevit@gmail.com>
2021-11-01 11:44:39 +0200

    Adding new Python folder

---
+++ Python (Basic Data Types)/Q1_List Comprehensions.py
@@ -0,0 +1,16 @@
+
+
+
+
+if __name__ == '__main__':
+    #Taking values
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+#Creating list with list comprehensions based on the requirements    
+lst= [[i, j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n ]
+
+#print list
+print(lst)
