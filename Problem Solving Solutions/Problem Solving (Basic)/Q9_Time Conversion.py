cdf0cb3e329054a98316871b81f75d381e43e0f8
Seqat <sedatalizevit@gmail.com>
2021-11-04 14:00:21 +0200

    Revise solution Q9

--- Problem Solving Solutions/Problem Solving (Basic)/Q9_Time Conversion.py
+++ Problem Solving Solutions/Problem Solving (Basic)/Q9_Time Conversion.py
@@ -12,3 +12,56 @@ Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 
 Sedat Ali ZEVİT
 """
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    x = s.split(":", 3)
+    a=x[2]
+    b=a[:2:]
+    a=a[2::]
+
+    x.remove(x[2])
+    x.append(b)
+
+    
+    print(x)
+    
+    if(int(x[0])<=11 and int(x[0])>=1 and a=="PM"):
+        x[0]=str(int(x[0])+12)
+        return(":".join(x))        
+    
+    elif(int(x[0])==12 and a=="AM"):
+        x[0]=str(int(x[0])-12) + "0"
+        
+        return(":".join(x))
+    
+    else:
+        return(":".join(x))
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
