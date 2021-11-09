087c7e6523a95d6bb59087c340d29488d943f024
Seqat <sedatalizevit@gmail.com>
2021-11-09 15:41:13 +0200

    new solution in c++

---
+++ C++ Solutions/C++ (Introduction) Solutions/Q5_For Loop.cpp
@@ -0,0 +1,61 @@
+/*
+HackerRank C (Introduction) Practice   
+Q5 : For Loop
+
+Sedat Ali ZEVİT
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    
+    int a,b;
+    int n;
+    
+    cin>>a>>b;
+    
+    for(n=a;n<=b; n++){
+    if (n == 1){
+        printf("one\n");
+    }
+    else if(n==2){
+        printf("two\n");        
+    }
+    else if(n==3){
+        printf("three\n");        
+    }
+    else if(n==4){
+        printf("four\n");        
+    }
+    else if(n==5){
+        printf("five\n");        
+    }
+    else if(n==6){
+        printf("six\n");        
+    }
+    else if(n==7){
+        printf("seven\n");        
+    }
+    else if(n==8){
+        printf("eight\n");        
+    }
+    else if(n==9){
+        printf("nine\n");        
+    }
+    else{
+        if(n%2==0){
+            printf("even\n");
+        }
+        else{
+            printf("odd\n");
+        }
+    }        
+        
+    }
+    
+    
+    return 0;
+}
