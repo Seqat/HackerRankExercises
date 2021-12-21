00c50ed173bb21c2da8bbc99c51c688cd5b2fd57
Sedat Ali Zevit <37043095+Seqat@users.noreply.github.com>
2021-12-21 15:19:48 +0200

    Adding Day3 Solutions

---
+++ 10 Days of Javascript/Day3/Arrays.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    
+    /// Takes the input array,
+    /// Filters the unique indexes (no reccurent values),
+    /// Sorts in decending order
+
+    var uniquearray = nums
+                        .filter((val, index, a) => a.indexOf(val) === index)
+                        .sort(function(a, b){return b-a});
+    
+
+    // Returns the second largest value
+    return uniquearray[1];
+    
+    
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
